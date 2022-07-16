b480fe7f5b734ae9518abf92b44145956b97158d
michael muliro <michael.muliro@student.moringaschool.com>
2022-07-16 10:17:20 +0300

    wueh lists and keys

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,26 @@
+
+
+
+
+
+
+
 import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const Iterate = links.map((link) => {
+    return <a key = {link} href = {link}>{link}</a>;
+  });
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{Iterate}</nav>;
 }
 
 export default NavBar;
+
+
+
+
+/*const colorElements = colors.map((color) => {
+  return <li style={{ color: color }}>{color}</li>;
+});*/
